10a6d40b7abc1b15860b553d8fd97cece104bc7c
Tommy Tran <tommy.tran@bcm.edu>
2022-10-30 14:17:11 -0500

    u

---
+++ Notes_THT.R
@@ -0,0 +1,62 @@
+Github logistics
+
+1637
+1360
+1579  
+1802
+
+1824
+1714
+1904
+1915
+
+percent difference 
+10.8
+23.0
+18.7
+6.1
+
+percent increase
+11.4
+26.0
+20.6
+6.3
+
+Comparing Pangenomes
+
+GH cores: FigS3, Cpr 1637, Cps 1360, Cac 1579, Ctu 1802
+Anvio cores: Cpr 1824, Cps 1714, 1904 Cac, 1915 Ctu
+https://klemonlab.github.io/CorPGA_Pangenomics_Metabolomics/SupplementalMethods_Anvio.html#anvio-v7
+
+GH pans: Cpr 3777, Cps 4978, Cac 4488, Ctu 3232
+
+Anvio pans: Cpr 3108, Cps 3590, Cac 3427, Ctu 2907
+
+percent difference: 
+19.4
+32.4
+26.8
+10.6
+
+@Tommy  - Info on genome sizes for other common nasal microbionts is in Steph’s mSystems paper. Please find that info (and any citations) to add here. Discussion: set genome size in the context of other common nasal microbionts and other Coryne species.
+
+THT: Do we have # already that would allow us to estimated what % of the core genes for each species are hypothetical vs what % of the accessory genes are hypothetical. 
+
+- move main outputs to github
+- go over main text 
+- core size of BWA vs USA strains Cps
+- add anvi'o metabolism to Github
+- do GTDBtk all Coryne strains plus 13 from SK
+- do ANI of all Coryne strains plus 13 from SK
+
+Figs (Potter)
+Fig. 1/ Fig. S1 Ari/Tommy
+Fig. 2 Andie/Giannina
+Fig. 3 Leah
+Fig. 4: Josh
+Fig. 5: Yuying/Andie
+
+Figs (Perry)
+Opine on graphical abstract: Tommy/Ari
+Fig 1. Josh
+Figure 2/3: Tommy/Ari (skip 3B)
